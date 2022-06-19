7be3fc7e7f34e776d0115bf5830edf545c111d77
Jin Kim <jinsungkim1210@gmail.com>
2022-06-19 10:24:43 -0700

    feat: framework

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+
+// Components
+import PostGrid from "./components/PostGrid";
+
+export default function App() {
+    return (
+      <div className="App">
+          <PostGrid />
+          <p>Hello</p>
+      </div>
+    );
+}
---
+++ src/components/PostGrid.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class PostGrid extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="grid">
+            
+            </div>
+        );
+    }
+}
