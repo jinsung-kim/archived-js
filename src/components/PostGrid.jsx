cf38d2c39c487501514581ef3ea3bbe483019b1e
Jin Kim <jinsungkim1210@gmail.com>
2022-06-26 10:50:47 -0700

    style: images scale appropriately

--- src/components/PostGrid.jsx
+++ src/components/PostGrid.jsx
@@ -10,33 +10,19 @@ export default class PostGrid extends Component {
 
         this.state = {
             posts: [],
-            width: window.innerWidth
         }
-    }
-
-    // Window resizing methods
-    handleResize = (e) => {
-        this.setState({ width: window.innerWidth });
-    }
-      
-    componentDidMount() {
-        window.addEventListener("resize", this.handleResize);
-    }
-      
-    componentWillUnmount() {
-        window.addEventListener("resize", this.handleResize);
     }
 
     render() {
         return (
             <div className="grid">
-                <PostThumbnail maxWidth={ this.state.width / 4 } 
+                <PostThumbnail 
                     title={"Virgil's SS 2022 Show"} url={ "https://swaggerareus.com/wp-content/uploads/2021/06/virgil-abloh-louis-vuitton-spring-summer-2022-23.jpeg" }/>
 
-                <PostThumbnail maxWidth={ this.state.width / 4 } 
+                <PostThumbnail
                     title={"Virgil's SS 2021 Show"} url={ "https://swaggerareus.com/wp-content/uploads/2021/06/virgil-abloh-louis-vuitton-spring-summer-2022-23.jpeg" }/>
 
-                <PostThumbnail maxWidth={ this.state.width / 4 } 
+                <PostThumbnail
                     title={"Virgil's SS 2020 Show"} url={ "https://swaggerareus.com/wp-content/uploads/2021/06/virgil-abloh-louis-vuitton-spring-summer-2022-23.jpeg" }/>
             </div>
         );
--- src/components/PostThumbnail.jsx
+++ src/components/PostThumbnail.jsx
@@ -9,16 +9,42 @@ export default class PostThumbnail extends Component {
         this.state = {
             title: props.title,
             thumbnail: props.url,
-            maxWidth: props.maxWidth,
-            maxHeight: props.maxHeight
+            width: window.innerWidth,
+            maxHeight: window.innerHeight
         };
+    }
+
+    // Window resizing methods
+    handleResize = (e) => {
+        this.setState({ width: window.innerWidth });
+    }
+      
+    componentDidMount = () => {
+        window.addEventListener("resize", this.handleResize);
+    }
+    
+    componentWillUnmount = () => {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    // Determines how to size the images
+    imageSize = () =>  {
+        var resWidth = 0;
+        if (this.state.width < 600) {
+            resWidth = this.state.width / 1.25;
+        } else if (this.state.width < 1000) {
+            resWidth = this.state.width / 3;
+        } else {
+            resWidth = this.state.width / 4;
+        }
+        return resWidth;
     }
 
     render() {
         return (
             <div className="post-box">
                 <img src={ this.state.thumbnail } alt="" 
-                style={{maxWidth: this.state.maxWidth, maxHeight: this.state.maxHeight}}
+                style={{ width: this.imageSize() }}
                 />
                 <h3>{ this.state.title }</h3>
             </div>
