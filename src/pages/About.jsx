e43da21598f051aa75dae50bad00fba874c94d53
Jin Kim <jinsungkim1210@gmail.com>
2022-08-09 15:25:30 -0700

    style

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -27,26 +27,26 @@ export default class About extends Component {
         return (
             <div className="about-container">
                 <Navbar />
-                
+
                 <div className="text-box">
                     {/* <div className="reactive" onMouseOver={ this.onHover }> */}
-                        <p className="title">archived.js</p>
+                    <p className="title">archived.js</p>
                     {/* </div> */}
                     <p className="sub">
                         archived.js is a creative writing project that I have envisioned with the intent to document my thinking process and
                         interaction with art. "archived" in this case refers to my vast collection of writing.
-                        The "js" in the second part refers to the initials of my first name (진성 or Jin Sung) while also referencing the framework 
+                        The "js" in the second part refers to the initials of my first name (진성 or Jin Sung) while also referencing the framework
                         this entire project (frontend and backend) was created in (Javascript).
-                        You can see how it was designed and created <a href="https://github.com/jinsung-kim/archived-js">here</a>. 
-                        
+                        You can see how it was designed and created <a href="https://github.com/jinsung-kim/archived-js">here</a>.
+
                         <br></br>
                         <br></br>
 
                         <i>Why does it need to exist?</i> This is the first question I always think of when I bring something to light.
-                        I have a page dedicated to writing about books that I have read <a href="https://jinkim.io/blog">here</a> - but I 
-                        found it rather one dimensional, and have been wanting to expand into other realms of interests that I have. I also 
+                        I have a page dedicated to writing about books that I have read <a href="https://jinkim.io/blog">here</a> - but I
+                        found it rather one dimensional, and have been wanting to expand into other realms of interests that I have. I also
                         go through many cycles of thoughts - but find myself often too distracted to dig deeper.
-                        With this project, I hope to be able to deliver more thought provoking ideas with clear cut examples so that I can reference 
+                        With this project, I hope to be able to deliver more thought provoking ideas with clear cut examples so that I can reference
                         them in my future works, and see my progression as a writer.
 
                         <br></br>
@@ -55,13 +55,13 @@ export default class About extends Component {
                         I am interested in many old fashion houses and more so the creatives that led them to great success. I am deeply
                         interested in exploring the likes of Martin Margiela and the underlying
                         principles that guide their work to where it is. I wish to look further into how they set their place in history while redefining
-                        everything else to come. I intend to focus on the storytelling elements of collections, 
+                        everything else to come. I intend to focus on the storytelling elements of collections,
                         the artfulness of which garments are presented, and the unique cuts of clothes alongside the culture impact that fashion has.
 
                         <br></br>
                         <br></br>
 
-                        <i>I believe that anything I don't write down will eventually be forgotten. In a way, this is my will to leave something behind.</i>
+                        <i>I believe that anything I don't write down will eventually be forgotten.</i>
                     </p>
 
                     <p className="title">contact</p>
